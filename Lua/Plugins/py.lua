3d8cccbe3bd97b9871e879c77088e28667117195
Face <69168154+face-hh@users.noreply.github.com>
2024-03-23 20:35:29 +0200

    Update Lua/Plugins/py.lua
    Co-authored-by: betapacketdlldink.nodllforyou <107498533+RealPacket@users.noreply.github.com>

--- Lua/Plugins/py.lua
+++ Lua/Plugins/py.lua
@@ -60,6 +60,21 @@ highlight("]", "binary")
 --- Strings
 highlight_region("'", "'", "string")
 highlight_region('"', '"', "string")
-
+--- (IDE) Comments
+add_comment("WHAT THE FUCK STOP")
+add_comment("this shi 100% breaking")
+add_comment("Waiter, waiter! More useless docstrings for this self-explanatory code!")
+add_comment("Tip: remove all spaces and tabs")
+add_comment("You have to avoid Python to make your script fast")
+add_comment("🚨🚨🚨 🐌🐌🐌 🚨🚨🚨")
+add_comment("Do NOT run Python on your computer. I did and ALMOST died")
+add_comment("Python is deprecated, uninstall python and use anything else instead.")
+add_comment("Stop using black, its name is racist!")
+add_comment(
+    "For more annoyance, please use PyLint and enable C0114, " 
+    .. "C0115, and C0116. You'll hate me for it.")
+add_comment(
+    "You have to document every variable, function, "
+    .. "module, and class in Python because self-explanatory code is not enough")
 --- Comments
 highlight_region("#", "", "comments", true)
