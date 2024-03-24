61032dd25587696f6fc71a58eaeb4d128bf2ef1b
uynilo9 <uynilo9@outlook.com>
2024-03-24 17:19:43 +0800

    Add the theme Catppuccin Latte

---
+++ Lua/Themes/Catppuccin Latte.lua
@@ -0,0 +1,20 @@
+set_keywords("reserved", "8839ef") --- Mauve
+set_keywords("string", "40a02b") --- Green
+set_keywords("binary", "fe640b") --- Peach
+set_keywords("symbol", "e64553") --- Maroon
+set_keywords("variable", "df8e1d") --- Yellow
+set_keywords("operator", "179299") --- Teal
+set_keywords("comments", "7c7f93") --- Overlay 2
+set_keywords("error", "d20f39") --- Red
+set_keywords("function", "1e66f5") --- Blue
+set_keywords("member", "209fb5") --- Sapphire
+
+
+set_gui("background_color", "eff1f5") --- Base
+set_gui("current_line_color", "acb0be") --- Surface 2
+set_gui("selection_color", "9ca0b0") --- Overlay 0
+set_gui("font_color", "4c4f69") --- Text
+set_gui("word_highlighted_color", "8c8fa1") --- Overlay 1
+set_gui("completion_background_color", "bcc0cc") --- Surface 1
+set_gui("completion_selected_color", "ccd0da") --- Surface 0
+set_gui("caret_color", "dd7878") --- Flamingo
