4263feb326d59264ced59b610de14af83a0629b7
uynilo9 <uynilo9@outlook.com>
2024-03-24 17:05:07 +0800

    Add the theme Catppuccin Mocha

---
+++ Lua/Themes/Catppuccin Mocha.lua
@@ -0,0 +1,20 @@
+set_keywords("reserved", "cba6f7") --- Mauve
+set_keywords("string", "a6e3a1") --- Green
+set_keywords("binary", "fab387") --- Peach
+set_keywords("symbol", "eba0ac") --- Maroon
+set_keywords("variable", "f9e2af") --- Yellow
+set_keywords("operator", "94e2d5") --- Teal
+set_keywords("comments", "9399b2") --- Overlay 2
+set_keywords("error", "f38ba8") --- Red
+set_keywords("function", "89b4fa") --- Blue
+set_keywords("member", "74c7ec") --- Sapphire
+
+
+set_gui("background_color", "1e1e2e") --- Base
+set_gui("current_line_color", "585b70") --- Surface 2
+set_gui("selection_color", "6c7086") --- Overlay 0
+set_gui("font_color", "cdd6f4") --- Text
+set_gui("word_highlighted_color", "7f849c") --- Overlay 1
+set_gui("completion_background_color", "45475a") --- Surface 1
+set_gui("completion_selected_color", "313244") --- Surface 0
+set_gui("caret_color", "f2cdcd") --- Flamingo
