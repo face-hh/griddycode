6e4fb35d274e509332251342f11f0022020ee766
uynilo9 <uynilo9@outlook.com>
2024-03-24 17:15:08 +0800

    Add the theme Catppuccin Frappe

---
+++ Lua/Themes/Catppuccin Frappe.lua
@@ -0,0 +1,20 @@
+set_keywords("reserved", "ca9ee6") --- Mauve
+set_keywords("string", "a6d189") --- Green
+set_keywords("binary", "ef9f76") --- Peach
+set_keywords("symbol", "ea999c") --- Maroon
+set_keywords("variable", "e5c890") --- Yellow
+set_keywords("operator", "81c8be") --- Teal
+set_keywords("comments", "949cbb") --- Overlay 2
+set_keywords("error", "e78284") --- Red
+set_keywords("function", "8caaee") --- Blue
+set_keywords("member", "85c1dc") --- Sapphire
+
+
+set_gui("background_color", "303446") --- Base
+set_gui("current_line_color", "626880") --- Surface 2
+set_gui("selection_color", "737994") --- Overlay 0
+set_gui("font_color", "c6d0f5") --- Text
+set_gui("word_highlighted_color", "838ba7") --- Overlay 1
+set_gui("completion_background_color", "51576d") --- Surface 1
+set_gui("completion_selected_color", "414559") --- Surface 0
+set_gui("caret_color", "eebebe") --- Flamingo
