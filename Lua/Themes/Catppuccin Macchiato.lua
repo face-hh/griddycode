4654c4ae0b74afe9e30223721b8a5414a9395e96
uynilo9 <uynilo9@outlook.com>
2024-03-24 17:09:53 +0800

    Add the theme Catppuccin Macchiato

---
+++ Lua/Themes/Catppuccin Macchiato.lua
@@ -0,0 +1,20 @@
+set_keywords("reserved", "c6a0f6") --- Mauve
+set_keywords("string", "a6da95") --- Green
+set_keywords("binary", "f5a97f") --- Peach
+set_keywords("symbol", "ee99a0") --- Maroon
+set_keywords("variable", "eed49f") --- Yellow
+set_keywords("operator", "8bd5ca") --- Teal
+set_keywords("comments", "939ab7") --- Overlay 2
+set_keywords("error", "ed8796") --- Red
+set_keywords("function", "8aadf4") --- Blue
+set_keywords("member", "7dc4e4") --- Sapphire
+
+
+set_gui("background_color", "24273a") --- Base
+set_gui("current_line_color", "5b6078") --- Surface 2
+set_gui("selection_color", "6e738d") --- Overlay 0
+set_gui("font_color", "cad3f5") --- Text
+set_gui("word_highlighted_color", "8087a2") --- Overlay 1
+set_gui("completion_background_color", "494d64") --- Surface 1
+set_gui("completion_selected_color", "363a4f") --- Surface 0
+set_gui("caret_color", "f0c6c6") --- Flamingo
