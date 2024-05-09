68b02bdfe92ec228597925aa43f2396ad183d2a1
Awbert <119314310+SweetyAngel@users.noreply.github.com>
2024-05-09 17:27:05 +0300

    Revert "Implemented fuzzy search and fixed some bugs"
    This reverts commit 9c6cea9e63c93a999d33766b46af3c3af948343d.

--- Scripts/file_dialog.gd
+++ Scripts/file_dialog.gd
@@ -15,8 +15,6 @@ var query: String = ""
 var search_limit: int = 1000
 var current_dirs_count: int = 0
 var handled: bool
-var max_coincidence: Array = []
-var coincidence: Array = []
 
 var zoom: Vector2;
 
@@ -64,13 +62,15 @@ func _input(event: InputEvent) -> void:
 	handled = true
 	if key_event.keycode == KEY_UP:
 		selected_index = max(0, selected_index - 1)
+		query = ""
 	elif key_event.keycode == KEY_DOWN:
-		selected_index = min(len(dirs) - 1, selected_index + 1)
+		selected_index = min(dirs.size() - 1, selected_index + 1)
+		query = ""
 	elif key_event.keycode == KEY_ENTER:
 		handle_enter_key()
+		query = ""
 	else:
 		handled = false
-
 	if current_dirs_count <= search_limit and !handled:
 		if key_event.keycode == KEY_BACKSPACE:
 			if len(query) > 0:
@@ -79,18 +79,16 @@ func _input(event: InputEvent) -> void:
 			query = ""
 		if len(key_event.as_text()) == 1:
 			query += key_event.as_text().to_lower()
-		elif key_event.keycode == KEY_PERIOD:
+		elif key_event.as_text().to_lower() == "period":
 			query += "."
-	max_coincidence = []
-
-	for i in range(1, len(dirs)):
-		if len(bbcode_dirs[i]) > 30:
-			continue
-		coincidence = fuzzy_search(dirs[i].to_lower(), query)
-		bbcode_dirs[i] = make_bold(dirs[i], coincidence)
-		if is_closer(max_coincidence, coincidence):
-			max_coincidence = coincidence
-			if not handled:	selected_index = i
+		if dirs[selected_index].to_lower().begins_with(query):
+			bbcode_dirs[selected_index] = "[b]"+bbcode_dirs[selected_index].left(len(query))+"[/b]"+bbcode_dirs[selected_index].right(len(dirs[selected_index])-len(query))
+		else:
+			for i in range(1, len(dirs)):
+				if dirs[i].to_lower().begins_with(query):
+					selected_index = i
+					bbcode_dirs[selected_index] = make_bold(dirs[selected_index], len(query))
+					break
 
 	update_ui()
 
@@ -99,7 +97,7 @@ func update_ui() -> void:
 	show_items()
 
 func show_items() -> void:
-	for i in range(len(bbcode_dirs)):
+	for i in range(bbcode_dirs.size()):
 		show_item(i)
 
 func show_item(index: int) -> void:
@@ -131,7 +129,7 @@ func show_item(index: int) -> void:
 
 	if is_dir: filename = bbcode_item
 
-	if len(filename) > 30:
+	if filename.length() > 30:
 		filename = filename.left(30) + "..." + filename.right(3)
 
 	if bbcode_item == "..":
@@ -153,7 +151,7 @@ func is_selected(item: String) -> bool:
 	return (is_dir_item and is_dir_current)
 
 func handle_enter_key() -> void:
-	if selected_index > len(dirs): return
+	if selected_index > dirs.size(): return
 	# ^^ this happens when the cursor was at, i.e., pos. 6, but arr is only has 4 entries
 
 	var item = dirs[selected_index];
@@ -178,42 +176,8 @@ func handle_enter_key() -> void:
 		change_dir(item)
 	update_ui()
 
-func make_bold(string: String, indexes: Array) -> String:
-	var new_string: String = ""
-
-	for i in range(len(string)):
-		if i in indexes: new_string += "[b]" + string[i] + "[/b]"
-		else: new_string += string[i]
-
-	return new_string
-
-func fuzzy_search(string: String, substring: String) -> Array:
-	if len(string) < len(substring): return []
-
-	var indexes: Array = []
-	var pos: int = -1
-
-	for letter in substring:
-		while pos < len(substring) - 1:
-			pos += 1
-			if string[pos] == letter:
-				indexes.append(pos)
-				break
-
-	return indexes if len(indexes) == len(query) else []
-
-# Compares 2 fuzzy Arrays and returns if 'new' Array is closer to query than 'old'
-func is_closer(old: Array, new: Array) -> bool:
-	if len(old) == 0: return true
-	if len(new) == 0: return false
-
-	if old == new: return false
-
-	for i in range(len(old)):
-		if old[i] > new[i]: return true
-		elif old[i] < new[i]: return false
-
-	return false
+func make_bold(string: String, size: int):
+	return "[b]" + string.left(len(query)) + "[/b]" + string.right(len(string)-len(query))
 
 # global_position is slightly off, so we customize it a little.
 func gp() -> Vector2:
