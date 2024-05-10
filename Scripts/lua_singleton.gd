ad1240fb0483793ac6e5e62fe1615f612069fa40
cookiemonsternz <Christopher.body@mcglashan.school.nz>
2024-05-11 00:07:25 +1200

    Added spectrum analyzer and toned down movement.

--- Scripts/lua_singleton.gd
+++ Scripts/lua_singleton.gd
@@ -358,7 +358,6 @@ func handle_internal_setting_change(property: String, value: Variant) -> void:
 		Music.set_volume(value)
 	if p == "music_move_intensity":
 		Music.music_move_intensity = value
-		print(value)
 	if p == "discord_sdk":
 		discord_sdk = value;
 
